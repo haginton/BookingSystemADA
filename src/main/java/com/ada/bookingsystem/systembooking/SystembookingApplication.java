ef4b43813c372a1d2ac40eeee0e2f9f1beb20ab3
Harrison Martinez <jonathan.martinez@corusconsulting.com>
2022-10-24 20:39:29 -0500

    First commit from Integrator Project

---
+++ src/main/java/com/ada/bookingsystem/systembooking/SystembookingApplication.java
@@ -0,0 +1,13 @@
+package com.ada.bookingsystem.systembooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystembookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystembookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ada/bookingsystem/systembooking/SystembookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.bookingsystem.systembooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystembookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
